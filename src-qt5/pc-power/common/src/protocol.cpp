4d70d71ced50e993a26c26a4967608edffee2acf
yurkis <yurkis@pcbsd.org>
2015-11-03 14:37:45 +0200

    pwrd: reduce amount of symbols in protocol messages

--- src-qt5/pc-power/common/src/protocol.cpp
+++ src-qt5/pc-power/common/src/protocol.cpp
@@ -4,5 +4,14 @@
 
 QString QJsonObjectToMessage(QJsonObject obj)
 {
-    return QJsonObject2String(obj).replace("\n"," ");
+    QString ret;
+    QString str = QJsonObject2String(obj);
+    int length = str.length();
+    for (int i=0; i<length; i++)
+    {
+        if ( (str[i] == ' ') || (str[i] == '\n') ) continue;
+        ret+=str[i];
+    }
+    return ret;
+    //return QJsonObject2String(obj).replace("\n"," ");
 }
