b4c14dbb0c7c5aae411315f05714a6e237acbb3f
yurkis <yurkis@pcbsd.org>
2015-11-03 15:00:01 +0200

    pwrd: revert protocol changes (was bad idea); better formatting for pwrcli

--- src-qt5/pc-power/common/src/protocol.cpp
+++ src-qt5/pc-power/common/src/protocol.cpp
@@ -4,14 +4,5 @@
 
 QString QJsonObjectToMessage(QJsonObject obj)
 {
-    QString ret;
-    QString str = QJsonObject2String(obj);
-    int length = str.length();
-    for (int i=0; i<length; i++)
-    {
-        if ( (str[i] == ' ') || (str[i] == '\n') ) continue;
-        ret+=str[i];
-    }
-    return ret;
-    //return QJsonObject2String(obj).replace("\n"," ");
+    return QJsonObject2String(obj).replace('\n',' ');
 }
